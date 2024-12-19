5d3729fc52a4a1f23f16380c7c8742b50c5cfde6
nt2311 <nguyentoan231196@gmail.com>
2024-12-19 23:23:39 +0700

    add clock and tick function

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from constants import *
 def main():
 
     pygame.init()
-
-    print("Starting asteroids!")
-
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SWSURFACE)
 
     while True:
@@ -16,8 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((255, 255, 255))
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":

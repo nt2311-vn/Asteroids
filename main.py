2c333611248c20c1d55c5aaeb18c816eec00cabb
nt2311 <nguyentoan231196@gmail.com>
2024-12-19 23:18:46 +0700

    complete make normal pygame function

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
 
+from constants import *
+
 
 def main():
+
+    pygame.init()
+
     print("Starting asteroids!")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SWSURFACE)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
